f60dacb31b1a591db6b17d4414b57cb80ef6cb2c
666bury <buryj666@gmail.com>
2019-02-07 12:30:32 +0100

    interacive visualization  plots completed

---
+++ 15.Interactive_Data_Visualization_with_Bokeh/4.8.Adding_a_hover_tool.py
@@ -0,0 +1,48 @@
+'''In this exercise, you'll practice adding a hover tool to drill down into data column values and display more detailed information about each scatter point.
+
+After you're done, experiment with the hover tool and see how it displays the name of the country when your mouse hovers over a point!
+
+The figure and slider have been created for you and are available in the workspace as plot and slider.'''
+#TASK
+# Import HoverTool from bokeh.models.
+# Create a HoverTool object called hover with tooltips=[('Country', '@country')].
+# Add the HoverTool object you created to the plot using add_tools().
+# Create a row layout using widgetbox(slider) and plot.
+# Add the layout to the current document. This has already been done for you.
+
+# Import HoverTool from bokeh.models
+
+
+# Create a HoverTool: hover
+hover = ____
+
+# Add the HoverTool to the plot
+
+# Create layout: layout
+layout = ____
+
+# Add layout to current document
+curdoc().add_root(layout)
+
+
+
+
+
+
+
+
+
+#SOLUTION
+# Import HoverTool from bokeh.models
+from bokeh.models import HoverTool
+
+# Create a HoverTool: hover
+hover = HoverTool(tooltips=[('Country', '@country')])
+
+# Add the HoverTool to the plot
+plot.add_tools(hover)
+# Create layout: layout
+layout = row(widgetbox(slider), plot)
+
+# Add layout to current document
+curdoc().add_root(layout)
